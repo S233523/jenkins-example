e7f288ea37624cbe72e44ff7bf3c113f48727bb4
S233523 <61187904+S233523@users.noreply.github.com>
2020-02-19 14:07:19 +0100

    Add files via upload

---
+++ JUnitln28Minutes-Master/src/main/java/org/JUnitln28Minutes/Master/App.java
@@ -0,0 +1,13 @@
+package org.JUnitln28Minutes.Master;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
